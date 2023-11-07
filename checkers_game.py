004bfdb117bb9f76a00db2a5de435c212a2f28c6
Tim Power <tjdpower@shaw.ca>
2023-11-06 19:10:30 -0700

    to add grid coordinates for easier identification

--- checkers_game.py
+++ checkers_game.py
@@ -49,41 +49,60 @@ R11 = Checkers_Piece("Red11", "red", (6,4))
 R12 = Checkers_Piece("Red12", "red", (6,2))
 
 #Building game board
-vertical_border = str("+-----" * 8) + "+"
+header_string = "    "
+for a in range(1,9):
+      header_string += str(a) + "     "
+header_string += "   "
+vertical_border = " " + str("+-----" * 8) + "+"
 
 odd_rows = str("|# # #|     " * 4) + "|"
 even_rows = str("|     |# # #" * 4) + "|"
 
+board.append(header_string)
 board.append(vertical_border)
-for i in range(8):
-      for j in range(3):
-            if i%2 == 0:
-                  board.append(odd_rows)
-            else:
-                  board.append(even_rows)
+for b in range(8):
+      if b%2 == 0:
+            board.append(" " + odd_rows)
+            board.append(str(b+1) + odd_rows)
+            board.append(" " + odd_rows)
+      else:
+            board.append(" " + even_rows)
+            board.append(str(b+1) + even_rows)
+            board.append(" " + even_rows)
       board.append(vertical_border)
 
 #placing red pieces on board
-for b in range(len(Checkers_Piece.red_piece_location_list)):
-      temp_string = ""
-      location = Checkers_Piece.red_piece_location_list[b]
-      if location = NULL:
-            temp_string = board[(location[0]*2 - 1) * 2]     
-      elif b < 9:
-            temp_string = board[(location[0]*2 - 1) * 2][:((location[1]*2 - 1) * 3)-2] + " R" + str(b+1) + "  " + board[(location[0]*2 - 1) * 2][((location[1]*2 - 1) * 3)+3:]
-      else:
-            temp_string = board[(location[0]*2 - 1) * 2][:((location[1]*2 - 1) * 3)-2] + " R" + str(b+1) + " " + board[(location[0]*2 - 1) * 2][((location[1]*2 - 1) * 3)+3:]      
-      board[(location[0]*2 - 1) * 2] = temp_string
+def place_red():
+      for d in range(len(Checkers_Piece.red_piece_location_list)):
+            temp_string = ""
+            location = Checkers_Piece.red_piece_location_list[d]
+            if location == "null":
+                  temp_string = board[location[0]*3 + location[0] - 1]     
+            elif d < 9:
+                  temp_string = board[location[0]*3 + location[0] - 1][:((location[1]*2 - 1) * 3)-1] + " R" + str(d+1) + "  " + board[location[0]*3 + location[0] - 1][((location[1]*2 - 1) * 3)+4:]
+            else:
+                  temp_string = board[location[0]*3 + location[0] - 1][:((location[1]*2 - 1) * 3)-1] + " R" + str(d+1) + " " + board[location[0]*3 + location[0] - 1][((location[1]*2 - 1) * 3)+4:]      
+            board[location[0]*3 + location[0] - 1] = temp_string
+
+place_red()
 
 #Placing black pieces on board
-for b in range(len(Checkers_Piece.black_piece_location_list)):
-      temp_string = ""
-      location = Checkers_Piece.black_piece_location_list[b]
-      if b < 9:
-            temp_string = board[(location[0]*2 - 1) * 2][:((location[1]*2 - 1) * 3)-2] + " B" + str(b+1) + "  " + board[(location[0]*2 - 1) * 2][((location[1]*2 - 1) * 3)+3:]
-      else:
-            temp_string = board[(location[0]*2 - 1) * 2][:((location[1]*2 - 1) * 3)-2] + " B" + str(b+1) + " " + board[(location[0]*2 - 1) * 2][((location[1]*2 - 1) * 3)+3:]      
-      board[(location[0]*2 - 1) * 2] = temp_string
+def place_black():
+      for e in range(len(Checkers_Piece.black_piece_location_list)):
+            temp_string = ""
+            location = Checkers_Piece.black_piece_location_list[e]
+            if location == "null":
+                  temp_string = board[location[0]*3 + location[0] - 1]     
+            elif e < 9:
+                  temp_string = board[location[0]*3 + location[0] - 1][:((location[1]*2 - 1) * 3)-1] + " B" + str(e+1) + "  " + board[location[0]*3 + location[0] - 1][((location[1]*2 - 1) * 3)+4:]
+            else:
+                  temp_string = board[location[0]*3 + location[0] - 1][:((location[1]*2 - 1) * 3)-1] + " B" + str(e+1) + " " + board[location[0]*3 + location[0] - 1][((location[1]*2 - 1) * 3)+4:]      
+            board[location[0]*3 + location[0] - 1] = temp_string
 
-for k in range(len(board)):
-      print(board[k])
+place_black()
+
+for f in range(len(board)):
+      print(board[f])
+
+
+#for testing
